60924e8340623a69760d393cb3300debc88178ea
Antoine Borda <pro@antoineborda.com>
2025-08-02 12:34:24 +0200

    Add ft_isspace.c file

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: aborda <aborda@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/30 18:45:41 by aborda            #+#    #+#             */
-/*   Updated: 2025/08/02 12:28:49 by aborda           ###   ########.fr       */
+/*   Updated: 2025/08/02 12:33:29 by aborda           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,5 +32,6 @@ int			ft_isalpha(int c);
 int			ft_isdigit(int c);
 int			ft_isalnum(int c);
 int			ft_isprint(int c);
+int			ft_isspace(int c);
 
 #endif
