a3a87ab01cce76ec2715ae85e4f8fc323728d593
globnot <antoineborda-pro@proton.me>
2025-10-11 20:04:10 +0200

    Add ft_tolower.c & test_ft_tolower.c files

--- ft_tolower.c
+++ ft_tolower.c
@@ -1,32 +1,20 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   test.c                                             :+:      :+:    :+:   */
+/*   ft_tolower.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: aborda <aborda@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/08/03 17:01:24 by aborda            #+#    #+#             */
-/*   Updated: 2025/10/11 20:01:45 by aborda           ###   ########.fr       */
+/*   Created: 2025/10/11 11:24:02 by aborda            #+#    #+#             */
+/*   Updated: 2025/10/11 19:58:39 by aborda           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "test.h"
+#include "libft.h"
 
-int	main(void)
+int	ft_tolower(int c)
 {
-	test_ft_isalpha();
-	test_ft_isdigit();
-	test_ft_isalnum();
-	test_ft_isascii();
-	test_ft_isprint();
-	test_ft_strlen();
-	test_ft_memset();
-	test_ft_bzero();
-	test_ft_memcpy();
-	test_ft_memmove();
-	test_ft_strlcpy();
-	test_ft_strlcat();
-	test_ft_toupper();
-	test_ft_tolower();
-	test_ft_atoi();
+	if (c >= 'A' && c <= 'Z')
+		c = c + 32;
+	return (c);
 }
--- test/test_ft_tolower.c
+++ test/test_ft_tolower.c
@@ -1,34 +1,34 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   test_ft_toupper.c                                  :+:      :+:    :+:   */
+/*   test_ft_tolower.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: aborda <aborda@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/11 15:07:56 by aborda            #+#    #+#             */
-/*   Updated: 2025/10/11 20:03:03 by aborda           ###   ########.fr       */
+/*   Updated: 2025/10/11 20:02:53 by aborda           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "test.h"
 
-void	test_ft_toupper(void)
+void	test_ft_tolower(void)
 {
 	t_test_int	tests[4];
 	size_t		nb_tests;
 	int			i;
 
-	tests[0] = (t_test_int){.actual = 'a', .label = "a"};
-	tests[1] = (t_test_int){.actual = 'A', .label = "A"};
+	tests[0] = (t_test_int){.actual = 'A', .label = "A"};
+	tests[1] = (t_test_int){.actual = 'a', .label = "a"};
 	tests[2] = (t_test_int){.actual = '0', .label = "0"};
 	tests[3] = (t_test_int){.actual = '\n', .label = "\\n"};
-	printf("\n========== Test de ft_toupper ==========\n");
+	printf("\n========== Test de ft_tolower ==========\n");
 	nb_tests = sizeof(tests) / sizeof(tests[0]);
 	i = 0;
 	while (nb_tests > 0)
 	{
-		assert_eq_int(toupper(tests[i].actual),
-			ft_toupper(tests[i].actual), tests[i].label);
+		assert_eq_int(tolower(tests[i].actual),
+			ft_tolower(tests[i].actual), tests[i].label);
 		i++;
 		nb_tests--;
 	}
